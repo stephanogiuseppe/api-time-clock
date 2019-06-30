aeb83f2f569ae12f0984877b1eccc856f84456bb
Stéphano Giuseppe <stephano.grp@gmail.com>
2019-06-30 18:21:35 -0300

    First commit

---
+++ src/main/java/com/example/timeclock/api/TimeClockApplication.java
@@ -0,0 +1,13 @@
+package com.example.timeclock.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeClockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeClockApplication.class, args);
+	}
+
+}
